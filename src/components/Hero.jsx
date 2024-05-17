2877ee353b3df4ebd5a2027ed749c9cbbeef841f
Navindu Abhishek <abi2000navi@gmail.com>
2024-05-17 20:53:17 +0530

    add hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,29 @@
+import heroImg from "../assets/hero_img.jpg";
+
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="flex flex-col md:flex-row items-center justify-between bg-cream p-8 md:p-16 gap-5">
+      <div className="md:w-1/2 text-center md:text-left mb-8 md:mb-0">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-dark-green mb-4 animate-fade-in-down">
+          Transform Your Body at StrengthX
+        </h1>
+        <p className="text-lg md:text-xl text-brown mb-6 animate-fade-in-up">
+          Welcome to StrengthX, where transformation happens! Our gym offers
+          cutting-edge equipment, personalized training programs, and a
+          motivating environment to push you beyond your limits.
+        </p>
+        <button
+          type="button"
+          className="text-gray-900 bg-gradient-to-r from-light-green via-tan to-brown hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-tan dark:focus:ring-dark-green font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-4"
+        >
+          Explore
+        </button>
+      </div>
+      <div className="md:w-1/2">
+        <img src={heroImg} alt="hero_image" className="w-full h-auto rounded-lg shadow-lg animate-fade-in" />
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
